158a058c92818d05c8e9cdf19350d03d04c9adb3
ITsverkunov <tsverkunovivan@gmail.com>
2023-02-16 21:06:11 +0300

    first commit

---
+++ app.ts
@@ -0,0 +1,37 @@
+interface ResData {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+class FifteenthTitle {
+  static #URL = 'https://jsonplaceholder.typicode.com/posts'
+  static  #ID = 15
+
+  static #get() {
+    fetch(FifteenthTitle.#URL)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        }
+      })
+      .then(resData => FifteenthTitle.#getTitleFromJson(resData))
+      .then(title => console.log(title))
+      .catch(({ message }) => console.error(message))
+  }
+
+  static #getTitleFromJson(resData: ResData[]) {
+    const itemWithTitle = resData.find(item => item.id === FifteenthTitle.#ID)
+    const title = itemWithTitle.title
+
+    return title
+  }
+
+  run() {
+    FifteenthTitle.#get()
+  }
+}
+
+const fifteenthTitle = new FifteenthTitle()
+fifteenthTitle.run()
